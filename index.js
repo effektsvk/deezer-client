27053b0c7b107961a759a3db1f464e21d069d515
Erik Slovak <erik.slovak7@gmail.com>
2016-06-14 15:46:49 +0200

    first working version

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const {app, Menu, Tray, BrowserWindow, electron} = require('electron');
-// const path = require('path');
+const path = require('path');
 
-app.commandLine.appendSwitch('ppapi-flash-path', 'pepflashplayer.dll');
+app.commandLine.appendSwitch('ppapi-flash-path', __dirname + '\\pepflashplayer.dll');
 app.commandLine.appendSwitch('ppapi-flash-version', '21.0.0.242');
 
 // let ppapi_flash_path;
@@ -11,12 +11,14 @@ app.commandLine.appendSwitch('ppapi-flash-version', '21.0.0.242');
 // On OS X, /path/to/PepperFlashPlayer.plugin
 // On Linux, /path/to/libpepflashplayer.so
 // if(process.platform  == 'win32'){
-//   ppapi_flash_path = path.join(__dirname, 'pepflashplayer.dll');
+//   ppapi_flash_path = path.join(__dirname, '/pepflashplayer.dll');
 // } else if (process.platform == 'linux') {
 //   ppapi_flash_path = path.join(__dirname, 'libpepflashplayer.so');
 // } else if (process.platform == 'darwin') {
 //   ppapi_flash_path = path.join(__dirname, 'PepperFlashPlayer.plugin');
 // }
+
+// let ppapi_flash_path = app.getPath('pepperFlashSystemPlugin');
 
 // app.commandLine.appendSwitch('ppapi-flash-path', ppapi_flash_path);
 
